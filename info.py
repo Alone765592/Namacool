b41df5812cbdc01b1e6eee51a1d95a82d46cb6e6
Alone <aloneboy765592@gmail.com>
2024-12-29 17:47:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,7 +55,7 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Jawan:My765592@cluster0.rbwks.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "cluster")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
 # Others
